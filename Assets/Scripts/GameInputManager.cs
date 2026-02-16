9e2c65886520089f5ec11c9f79a24f6d63a756b9
chris8jarvis <christinaeduardovna8@gmail.com>
2026-02-16 15:08:45 +0300

    Controls (task10) with action Game/Restart generated, GameInputManager script and Controls.inputActions made

---
+++ Assets/Scripts/GameInputManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInputManager : MonoBehaviour
+{
+    private Controls controls;
+    
+    private void Awake()
+    {
+        controls = new Controls();
+    }
+    
+    private void OnEnable()
+    {
+        controls.Game.Restart.performed += OnRestartPerformed;
+        controls.Game.Enable();
+    }
+    
+    private void OnDisable()
+    {
+        controls.Game.Restart.performed -= OnRestartPerformed;
+        controls.Game.Disable();
+    }
+    
+    private void OnRestartPerformed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Restart pressed!");
+        //
+    }
+}
