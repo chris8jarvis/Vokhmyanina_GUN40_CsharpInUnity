e018b81e4424a9c1f9a6b17b254091e46d04046a
chris8jarvis <christinaeduardovna8@gmail.com>
2026-02-17 12:16:40 +0300

    script Primitives with enums created

---
+++ Assets/Scripts/Primitives.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+public enum NeighbourType
+{
+    None = 0,           // Нет соседей (одиночная клетка)
+    Left = 1,           // Слева есть клетка
+    Right = 2,          // Справа есть клетка
+    Top = 3,            // Сверху есть клетка
+    Bottom = 4,         // Снизу есть клетка
+    LeftRight = 5,      // Слева и справа
+    TopBottom = 6,      // Сверху и снизу
+    Cross = 7,          // Все четыре стороны
+}
+
+public enum Team
+{
+    Player1 = 0,
+    Player2 = 1
+}
