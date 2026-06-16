ba4d87ed3a5ba61f4dc6074b2caa26f5987008c2
chris8jarvis <christinaeduardovna8@gmail.com>
2026-06-16 17:31:34 +0300

    work with enemy

--- Assets/Scripts/Ai/AiChasePlayerState.cs
+++ Assets/Scripts/Ai/AiChasePlayerState.cs
@@ -17,24 +17,24 @@ public class AiChasePlayerState : AiState
     }
 
     public void Update(AiAgent agent) {
-        if (!agent.enabled) {
-            return;
-        }
+         if (!agent.enabled) {
+             return;
+         }
 
-        timer -= Time.deltaTime;
-        if (!agent.navMeshAgent.hasPath) {
-            agent.navMeshAgent.destination = agent.playerTransform.position;
-        }
+         timer -= Time.deltaTime;
+         if (!agent.navMeshAgent.hasPath) {
+             agent.navMeshAgent.destination = agent.playerTransform.position;
+         }
 
-        if (timer < 0.0f) {
-            Vector3 direction = (agent.playerTransform.position - agent.navMeshAgent.destination);
-            direction.y = 0;
-            if (direction.sqrMagnitude > agent.config.maxDistance * agent.config.maxDistance) {
-                if (agent.navMeshAgent.pathStatus != NavMeshPathStatus.PathPartial) {
-                    agent.navMeshAgent.destination = agent.playerTransform.position;
-                }
-            }
-            timer = agent.config.maxTime;
+         if (timer < 0.0f) {
+             Vector3 direction = (agent.playerTransform.position - agent.navMeshAgent.destination);
+             direction.y = 0;
+             if (direction.sqrMagnitude > agent.config.maxDistance * agent.config.maxDistance) {
+                 if (agent.navMeshAgent.pathStatus != NavMeshPathStatus.PathPartial) {
+                     agent.navMeshAgent.destination = agent.playerTransform.position;
+                 }
+             }
+             timer = agent.config.maxTime;
         }
     }
 
